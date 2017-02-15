99914d4511c1a08c9428a8c95e0e462c9856aef3
jpalomon <j.palomon@alumnos.urjc.es>
2017-02-15 14:41:10 +0100

    Añadido HttpSession y incluidos mensajes de error en consultas, registros e inscripciones

---
+++ JugadorRepository.java
@@ -0,0 +1,17 @@
+package es.urjc.code.dad;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JugadorRepository extends JpaRepository <Jugador, Long>{
+
+	Jugador findByNombreJugador(String nombreJugador);
+	List<Jugador> findByEdad(int edad);
+	List<Jugador> findByNacionalidadJugador(String nacionalidadJugador);
+	
+	List<Jugador> findDistinctJugadoresByEquipoJugadorOrderByValorMercadoDesc(String equipoJugador);
+	List<Jugador> findDistinctJugadoresByNacionalidadJugadorOrderByValorMercadoDesc(String nacionalidadJugador);
+	List<Jugador> findDistinctJugadoresByEdadOrderByValorMercadoDesc(int edad);
+
+}
