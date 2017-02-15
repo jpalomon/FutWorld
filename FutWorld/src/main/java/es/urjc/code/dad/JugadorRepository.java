e3dc26e6e18f230b4eaf25ab3d366d673cb8cdbd
jpalomon <j.palomon@alumnos.urjc.es>
2017-02-15 15:46:44 +0100

    Añadido HttpSession e incluidos mensajes de error en consultas, registros e inscripciones

--- FutWorld/src/main/java/es/urjc/code/dad/JugadorRepository.java
+++ FutWorld/src/main/java/es/urjc/code/dad/JugadorRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface JugadorRepository extends JpaRepository <Jugador, Long>{
 
 	Jugador findByNombreJugador(String nombreJugador);
+	List<Jugador> findByEdad(int edad);
+	List<Jugador> findByNacionalidadJugador(String nacionalidadJugador);
 	
 	List<Jugador> findDistinctJugadoresByEquipoJugadorOrderByValorMercadoDesc(String equipoJugador);
 	List<Jugador> findDistinctJugadoresByNacionalidadJugadorOrderByValorMercadoDesc(String nacionalidadJugador);
